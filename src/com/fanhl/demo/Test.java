c0427eb658b72aef0691589f2c45fc1324bea702
fanhl <dyguests@qq.com>
2017-11-13 15:10:40 +0800

    fix

--- src/com/fanhl/demo/Test.java
+++ src/com/fanhl/demo/Test.java
@@ -1,10 +1,15 @@
 package com.fanhl.demo;
 
 public class Test {
-    void a() {
+    void aaaaaa() {
 
     }
 
+    /**
+     * 好看
+     *
+     * @param args
+     */
     void foo(String args[]) {
         System.out.println(args);
     }
